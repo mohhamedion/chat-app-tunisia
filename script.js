6dbd36a57c6cfba3cfed17bc2ab6f74acd0dee3b
unknown <mohhamedion@gmail.com>
2020-04-13 11:06:20 +0100

    make it better

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const express  = require("express");
 const app  = express();
-const port = 8000 || process.env.PORT;
+const port = process.env.PORT||8000 ;
 const server = app.listen(port,()=>{
     console.log('working on '+port)
 })
